a91e70ffa747b119410fdfd492424b0200d79dab
Raunak <rsingh4work@gmail.com>
2019-10-21 23:32:59 +0530

    Adds ApiLogger.

---
+++ Logger/ApiLogger.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Logger
+{
+    public class ApiLog : LogBase
+    {
+        public override string Type { get => "api"; }
+        public override List<KeyValuePair<string, object>> GetData()
+        {
+            var data = base.GetData();
+            data.Add(new KeyValuePair<string, object>("type", this.Type));
+            return data;
+        }
+    }
+}
