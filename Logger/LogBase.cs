0a2815a293756f1991cd8b5a649ebf8a469189ba
Raunak <rsingh4work@gmail.com>
2019-10-21 23:31:19 +0530

    Adds Logbase.

---
+++ Logger/LogBase.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Logger
+{
+    public abstract class LogBase
+    {
+        public string SessionId { get; set; }
+        public abstract string Type { get; }
+        public string ApplicationName { get; set; }
+        public string Method { get; set; }
+        public virtual List<KeyValuePair<string, object>> GetData()
+        {
+            var data = new List<KeyValuePair<string, object>>
+            {
+                new KeyValuePair<string, object>("session_id", this.SessionId),
+                new KeyValuePair<string, object>("type", this.Type),
+                new KeyValuePair<string, object>("application_name", this.ApplicationName),
+                new KeyValuePair<string, object>("method", this.Method)
+            };
+            return data;
+        }
+    }
+}
