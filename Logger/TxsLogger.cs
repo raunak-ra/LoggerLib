e74788704385f67abdee0b21fee19e2aa731e5e6
Raunak <rsingh4work@gmail.com>
2019-10-21 23:23:09 +0530

    Adds TxsLogger.

---
+++ Logger/TxsLogger.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Logger
+{
+    public class TxsLogger : ITxsLogger
+    {
+        public void InitLogger()
+        {
+            Log.Logger = new LoggerConfiguration()
+                         .MinimumLevel.Information()
+                         .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+                         .Enrich.FromLogContext()
+                         .WriteTo.Logger(l => l.Filter.ByIncludingOnly(e => e.Level == LogEventLevel.Verbose).WriteTo.RollingFile(@"Logs/Verbose/Verbose-{Date}.log"))
+                         .WriteTo.Logger(l => l.Filter.ByIncludingOnly(e => e.Level == LogEventLevel.Debug).WriteTo.RollingFile(@"Logs/Debug/Debug-{Date}.log"))
+                         .WriteTo.Logger(l => l.Filter.ByIncludingOnly(e => e.Level == LogEventLevel.Information).WriteTo.RollingFile(@"Logs/Api/Api-{Date}.log"))
+                         .WriteTo.Logger(l => l.Filter.ByIncludingOnly(e => e.Level == LogEventLevel.Warning).WriteTo.RollingFile(@"Logs/Warning/Warning-{Date}.log"))
+                         .WriteTo.Logger(l => l.Filter.ByIncludingOnly(e => e.Level == LogEventLevel.Error).WriteTo.RollingFile(@"Logs/Error/Error-{Date}.log"))
+                         .WriteTo.Logger(l => l.Filter.ByIncludingOnly(e => e.Level == LogEventLevel.Fatal).WriteTo.RollingFile(@"Logs/Fatal/Fatal-{Date}.log"))
+                         .CreateLogger();
+        }
+
+
+        public async Task WriteLogAsync(LogBase logger)
+        {
+            var data = logger.GetData();
+
+            if (logger is ErrorLogger)
+            {
+                Log.Error("{@data}", data);
+            }
+            else
+            {
+                Log.Information("{@data}", data);
+            }
+
+        }
+
+    }
+}
