42371249fc06a66984d53fbc38db0026dce9cd5b
Raunak <rsingh4work@gmail.com>
2019-10-22 12:35:03 +0530

    Add testing for api logger.

--- Logger.Tests/LoggerFixture.cs
+++ Logger.Tests/LoggerFixture.cs
@@ -5,10 +5,19 @@ namespace Logger.Tests
 {
     public class LoggerFixture
     {
-        [Fact]
-        public void Test1()
-        {
+        private readonly ITxsLogger l = new TxsLogger();
+        public string PropA { get; set; }
+        public string PropB { get; set; }
 
+        [Fact]
+        public void Testing_for_apilogger()
+        {
+            var log = new ApiLogger();
+            log.ApplicationName = "apiName";
+            log.Method = "getvalue";
+            log.SessionId = "12ert345rgftfsb877";
+            l.WriteLogAsync(log);
+            Assert.Equal(5, log.GetData().Count);
         }
     }
 }
