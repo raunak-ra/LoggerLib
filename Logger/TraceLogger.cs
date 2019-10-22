7ec0cf2b21bb076ea2ff9e3a1a513a40c213de08
Raunak <rsingh4work@gmail.com>
2019-10-22 12:29:51 +0530

    Update time elapsed.

--- Logger/TraceLogger.cs
+++ Logger/TraceLogger.cs
@@ -9,7 +9,7 @@ namespace Logger
         public override string Type { get => "trace"; }
         public string Message { get; set; }
         public string Status { get; set; }
-        public string TimeElapsed { get; set; }
+        public string TimeElapsedInMilliSeconds { get; set; }
 
         public override List<KeyValuePair<string, object>> GetData()
         {
@@ -17,7 +17,7 @@ namespace Logger
             data.Add(new KeyValuePair<string, object>("type", this.Type));
             data.Add(new KeyValuePair<string, object>("message", this.Message));
             data.Add(new KeyValuePair<string, object>("status", this.Status));
-            data.Add(new KeyValuePair<string, object>("time_elapsed", this.TimeElapsed));
+            data.Add(new KeyValuePair<string, object>("time_elapsed", this.TimeElapsedInMilliSeconds));
             return data;
         }
     }
