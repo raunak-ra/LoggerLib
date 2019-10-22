46f8a3768ac99a0575aec8c657f72cafa5467ab3
Raunak <rsingh4work@gmail.com>
2019-10-22 13:39:32 +0530

    Update api logger.

--- Logger/ApiLogger.cs
+++ Logger/ApiLogger.cs
@@ -7,10 +7,17 @@ namespace Logger
     public class ApiLogger : LogBase
     {
         public override string Type { get => "api"; }
+
+        public string Request { get; set; }
+        public string Response { get; set; }
+        public string Status { get; set; }
         public override List<KeyValuePair<string, object>> GetData()
         {
             var data = base.GetData();
             data.Add(new KeyValuePair<string, object>("type", this.Type));
+            data.Add(new KeyValuePair<string, object>("request", this.Request));
+            data.Add(new KeyValuePair<string, object>("response", this.Response));
+            data.Add(new KeyValuePair<string, object>("status", this.Status));
             return data;
         }
     }
