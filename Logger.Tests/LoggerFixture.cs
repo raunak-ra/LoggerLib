30640e9b28df21a02adcd0aa76ebd35150e37a68
Raunak <rsingh4work@gmail.com>
2019-10-24 10:34:05 +0530

    Update object name.

--- Logger.Tests/LoggerFixture.cs
+++ Logger.Tests/LoggerFixture.cs
@@ -6,7 +6,7 @@ namespace Logger.Tests
 {
     public class LoggerFixture
     {
-        private readonly ITxsLogger l = new TxsLogger();
+        private readonly ITxsLogger logger = new TxsLogger();
         public string PropA { get; set; }
         public string PropB { get; set; }
 
@@ -17,7 +17,7 @@ namespace Logger.Tests
             log.ApplicationName = "apiName";
             log.Method = "getvalue";
             log.SessionId = "12ert345rgftfsb877";
-            l.WriteLogAsync(log);
+            logger.WriteLogAsync(log);
             Assert.Equal(5, log.GetData().Count);
         }
 
@@ -34,7 +34,7 @@ namespace Logger.Tests
 
             log.keyValuePair.Add(new KeyValuePair<string, object>("prop_a", this.PropA));
             log.keyValuePair.Add(new KeyValuePair<string, object>("prop_b", this.PropB));
-            l.WriteLogAsync(log);
+            logger.WriteLogAsync(log);
             Assert.Equal(7, log.GetData().Count);
         }
 
@@ -49,7 +49,7 @@ namespace Logger.Tests
             log.Method = "getvalue";
             log.SessionId = "12ert345rgftfsb877";
 
-            l.WriteLogAsync(log);
+            logger.WriteLogAsync(log);
             Assert.Equal(9, log.GetData().Count);
         }
 
@@ -64,7 +64,7 @@ namespace Logger.Tests
             log.Message = "calling sorting method";
             log.Method = "method A";
             log.TimeElapsedInMilliSeconds = "12ms";
-            l.WriteLogAsync(log);
+            logger.WriteLogAsync(log);
             Assert.Equal(8, log.GetData().Count);
         }
     }
