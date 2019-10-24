c248f1d6072d08f9e61c58113fd7d401e4d40408
Raunak <rsingh4work@gmail.com>
2019-10-24 14:49:55 +0530

    Remove verbose and debug log filter.

--- Logger/TxsLogger.cs
+++ Logger/TxsLogger.cs
@@ -13,10 +13,8 @@ namespace Logger
         {
             Log.Logger = new LoggerConfiguration()
                          .MinimumLevel.Information()
-                         .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
-                         .Enrich.FromLogContext()
-                         .WriteTo.Logger(l => l.Filter.ByIncludingOnly(e => e.Level == LogEventLevel.Verbose).WriteTo.RollingFile(@"Logs/Verbose/Verbose-{Date}.log"))
-                         .WriteTo.Logger(l => l.Filter.ByIncludingOnly(e => e.Level == LogEventLevel.Debug).WriteTo.RollingFile(@"Logs/Debug/Debug-{Date}.log"))
+                         .MinimumLevel.Override("Microsoft", LogEventLevel.Information) 
+                         .Enrich.FromLogContext()            
                          .WriteTo.Logger(l => l.Filter.ByIncludingOnly(e => e.Level == LogEventLevel.Information).WriteTo.RollingFile(@"Logs/Api/Api-{Date}.log"))
                          .WriteTo.Logger(l => l.Filter.ByIncludingOnly(e => e.Level == LogEventLevel.Warning).WriteTo.RollingFile(@"Logs/Warning/Warning-{Date}.log"))
                          .WriteTo.Logger(l => l.Filter.ByIncludingOnly(e => e.Level == LogEventLevel.Error).WriteTo.RollingFile(@"Logs/Error/Error-{Date}.log"))
